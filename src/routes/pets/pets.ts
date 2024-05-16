f42e2a9ee9ffae6b0e7ddedecbc038535ce99513
Lucas Martins <lucasmbtos@gmail.com>
2024-05-16 16:53:34 -0300

    pet routes post and shelters get

--- src/routes/pets/pets.ts
+++ src/routes/pets/pets.ts
@@ -1,34 +1,28 @@
 import { FastifyInstance } from 'fastify';
 import { PETS } from '../../mocks';
-import {} from '../shelters/shelters';
 
 module.exports = async function (fastify: FastifyInstance) {
-  // fastify.route({
-  //   method: 'GET',
-  //   url: '/',
-  //   handler: function (request, reply) {
-  //     reply.send(PETS);
-  //   }
-  // });
-
-  // schema: {
-  //   querystring: {
-  //     type: 'object',
-  //     properties: {
-  //       shelter_id: {
-  //         type: 'string',
-  //         default: 1
-  //       }
-  //     }
-  //   }
-  // },
   fastify.route({
-    method: 'GET',
+    method: 'POST',
     url: '/',
     handler: function (request, reply) {
-      console.log(request.query);
-      console.log(request.params);
-      reply.send(PETS);
+      const shelter: number = request.body?.shelter_id;
+      const tags: Array<string> = request.body?.tags;
+      console.log({ shelter, tags });
+
+      // mock data section
+      var filtered_pets = PETS.filter((el) => el.shelter_id == shelter);
+
+      if (tags) {
+        tags.forEach((t) => {
+          filtered_pets = filtered_pets.filter(
+            (el1) => el1?.pet_tag_names.indexOf(t) == 0
+          );
+        });
+      }
+
+      reply.send(filtered_pets);
+      // mock data section
     }
   });
 };
