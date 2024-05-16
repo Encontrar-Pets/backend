35d4e92b3af370d0edc5da890c70548b159b3b56
Tiago Lopes <tiago_lopes58@msn.com>
2024-05-16 17:06:09 -0300

    fix db model

--- prisma/migrations/20240516200246_initial/migration.sql
+++ prisma/migrations/20240516200246_initial/migration.sql
@@ -23,14 +23,12 @@ CREATE TABLE "applicant_owner" (
 );
 
 -- CreateTable
-CREATE TABLE "characteristics" (
+CREATE TABLE "pet_tags" (
     "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "characteristics_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "pet_tags_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -41,21 +39,12 @@ CREATE TABLE "pets" (
     "type" TEXT NOT NULL,
     "temp_home_id" TEXT,
     "applicant_owner_id" TEXT,
+    "shelter_id" TEXT,
+    "img_url" TEXT,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "pets_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "pet_shelter" (
-    "id" TEXT NOT NULL,
-    "pet_id" TEXT NOT NULL,
-    "shelter_id" TEXT NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "pet_shelter_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -73,16 +62,30 @@ CREATE TABLE "shelter" (
 );
 
 -- CreateTable
-CREATE TABLE "_characteristicsTopets" (
+CREATE TABLE "pet_owner" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "pet_name" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "pet_owner_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_pet_tagsTopets" (
     "A" TEXT NOT NULL,
     "B" TEXT NOT NULL
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "_characteristicsTopets_AB_unique" ON "_characteristicsTopets"("A", "B");
+CREATE UNIQUE INDEX "_pet_tagsTopets_AB_unique" ON "_pet_tagsTopets"("A", "B");
 
 -- CreateIndex
-CREATE INDEX "_characteristicsTopets_B_index" ON "_characteristicsTopets"("B");
+CREATE INDEX "_pet_tagsTopets_B_index" ON "_pet_tagsTopets"("B");
 
 -- AddForeignKey
 ALTER TABLE "pets" ADD CONSTRAINT "pets_temp_home_id_fkey" FOREIGN KEY ("temp_home_id") REFERENCES "temp_home"("id") ON DELETE SET NULL ON UPDATE CASCADE;
@@ -91,13 +94,10 @@ ALTER TABLE "pets" ADD CONSTRAINT "pets_temp_home_id_fkey" FOREIGN KEY ("temp_ho
 ALTER TABLE "pets" ADD CONSTRAINT "pets_applicant_owner_id_fkey" FOREIGN KEY ("applicant_owner_id") REFERENCES "applicant_owner"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "pet_shelter" ADD CONSTRAINT "pet_shelter_pet_id_fkey" FOREIGN KEY ("pet_id") REFERENCES "pets"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "pets" ADD CONSTRAINT "pets_shelter_id_fkey" FOREIGN KEY ("shelter_id") REFERENCES "shelter"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "pet_shelter" ADD CONSTRAINT "pet_shelter_shelter_id_fkey" FOREIGN KEY ("shelter_id") REFERENCES "shelter"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "_pet_tagsTopets" ADD CONSTRAINT "_pet_tagsTopets_A_fkey" FOREIGN KEY ("A") REFERENCES "pet_tags"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "_characteristicsTopets" ADD CONSTRAINT "_characteristicsTopets_A_fkey" FOREIGN KEY ("A") REFERENCES "characteristics"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "_characteristicsTopets" ADD CONSTRAINT "_characteristicsTopets_B_fkey" FOREIGN KEY ("B") REFERENCES "pets"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "_pet_tagsTopets" ADD CONSTRAINT "_pet_tagsTopets_B_fkey" FOREIGN KEY ("B") REFERENCES "pets"("id") ON DELETE CASCADE ON UPDATE CASCADE;
