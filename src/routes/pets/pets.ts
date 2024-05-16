3bca60b282309e1ed044bd59be2b0d305a1a3fc5
Lucas Martins <lucasmbtos@gmail.com>
2024-05-16 17:23:35 -0300

    checkpoint routes and mocks

--- src/routes/pets/pets.ts
+++ src/routes/pets/pets.ts
@@ -3,21 +3,29 @@ import { PETS } from '../../mocks';
 
 module.exports = async function (fastify: FastifyInstance) {
   fastify.route({
-    method: 'POST',
+    method: 'GET',
     url: '/',
     handler: function (request, reply) {
-      const shelter: number = request.body?.shelter_id;
-      const tags: Array<string> = request.body?.tags;
-      console.log({ shelter, tags });
+      const shelter: number = request.query?.shelter_id;
+      var tags: Array<string> | undefined = undefined;
+      if (request.query?.tags) {
+        tags = request.query?.tags.split(',');
+      }
+      console.log({ shelter });
 
       // mock data section
       var filtered_pets = PETS.filter((el) => el.shelter_id == shelter);
 
       if (tags) {
+        // tags = tags.split(',');
+        console.log({ tags });
+        console.log(filtered_pets.length);
         tags.forEach((t) => {
-          filtered_pets = filtered_pets.filter(
-            (el1) => el1?.pet_tag_names.indexOf(t) == 0
-          );
+          console.log(filtered_pets.length);
+          filtered_pets = filtered_pets.filter((el1) => {
+            console.log(el1.name);
+            return el1?.pet_tag_description.indexOf(t) == 0;
+          });
         });
       }
 
