7ecd12c6380a3a07d7a53e14200ba255b51b038f
Bruno Cardoso <bruno.soarescardoso@outlook.com>
2024-05-17 19:06:00 -0300

    add cors and helmet

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,18 @@ import path from 'path';
 import AutoLoad from '@fastify/autoload';
 import { RequestListener, ServerResponse } from 'http';
 import { FastifyRequest, FastifyReply } from 'fastify';
+import { logger } from './services/logger';
 
 const fastify = require('fastify')({
+  logger: {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        translateTime: 'HH:MM:ss Z',
+        ignore: 'pid,hostname'
+      }
+    }
+  },
   serializers: {
     res(reply: FastifyReply) {
       return {
@@ -27,6 +37,10 @@ const fastify = require('fastify')({
 });
 
 fastify.register(AutoLoad, {
+  dir: path.join(__dirname, 'plugins')
+});
+
+fastify.register(AutoLoad, {
   dir: path.join(__dirname, 'routes')
 });
 
@@ -40,7 +54,7 @@ fastify.addContentTypeParser(
 fastify
   .listen({ port: 8080 })
   .then(async () => {
-    console.log(`Server Listening on Port ${process.env.PORT}`);
+    logger.info(`Server Listening on Port ${process.env.PORT}`);
   })
   .catch((err: Error) => {
     console.log(err);
