600c9e1ce51384ee9c7959e52204574325e0ab97
Bruno Cardoso <bruno.soarescardoso@outlook.com>
2024-05-17 19:52:56 -0300

    allow cors

--- src/plugins/cors.ts
+++ src/plugins/cors.ts
@@ -4,10 +4,15 @@ import { FastifyInstance } from 'fastify';
 import fp from 'fastify-plugin';
 
 /**
- * This plugins enables the use of CORS in a Fastify application.
+ * This plugin enables the use of CORS in a Fastify application.
  *
  * @see https://github.com/fastify/fastify-cors
  */
 module.exports = fp(async function (fastify: FastifyInstance) {
-  fastify.register(require('@fastify/cors'));
+  fastify.register(require('@fastify/cors'), {
+    origin: (origin, callback) => {
+      callback(null, true);
+    },
+    credentials: true
+  });
 });
