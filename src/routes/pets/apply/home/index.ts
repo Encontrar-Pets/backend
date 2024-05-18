f82c5047dbbc8dc5da26e88a8c417eb2d150911c
Lucas Martins <lucasmbtos@gmail.com>
2024-05-18 18:34:11 -0300

    structure of routes

--- src/routes/pets/apply/home/index.ts
+++ src/routes/pets/apply/home/index.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from 'fastify';
 
-import { applyHomeHandler } from '../../../controllers/pets';
+import { applyHomeHandler } from '../../../../controllers/pets';
 
 module.exports = async function (fastify: FastifyInstance) {
   fastify.route({
