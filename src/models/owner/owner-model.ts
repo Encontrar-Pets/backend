167e452354bd0dcba97175688597f03178f64e1a
Tiago Lopes <tiago_lopes58@msn.com>
2024-05-18 14:30:44 -0300

    fix param name

--- src/models/owner/owner-model.ts
+++ src/models/owner/owner-model.ts
@@ -4,11 +4,11 @@ import { OwnerDTO } from "./owner-dto";
 export class OwnerRepository {
     constructor(private readonly prisma: PrismaClient) { }
 
-    async create(aplicant: OwnerDTO) {
+    async create(owner: OwnerDTO) {
         return await this.prisma.applicant_owner.create({
             data: {
-                name: aplicant.name,
-                phone: aplicant.phone
+                name: owner.name,
+                phone: owner.phone
             }
         });
     }
